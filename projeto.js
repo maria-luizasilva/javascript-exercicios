df25f4f1e69f8a473cb4846fb0aebaaff41326e1
maria-luizasilva <74742506+maria-luizasilva@users.noreply.github.com>
2020-11-24 22:57:23 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,26 @@
+
+function botao(){
+   document.getElementById("agradecimento").innerHTML = "obrigada por clicar, cuide de seu viros, o avaste já sera instalado (~  w~)"
+};
+function bot(){
+    document.getElementById("agradecimen").innerHTML = "nao pode <b>disasumir</b> de sua responsabilidade menino >:( trate de alimenta-lo muito bem."
+};
+function opa(){
+    document.getElementById("agarde").innerHTML = " baixe mais amiginhos para ele e o alimente frequentimente com os seus dados pessoas :) ele é uma criança bobinha e se chama jesico"
+};
+
+function redireci(){
+
+    window.open  = "https://www.youtube.com/watch?v=MPmD6hFaomk&ab_channel=EDMBot"
+
+};
+
+function mause(){
+
+alert("trocar texto")
+//document.getElementById("mause"),innerHTML =" obrigado por passar o mause";
+}
+function muve(){
+    document.getElementById("e").innerHTML= "obrigada"
+}
+
